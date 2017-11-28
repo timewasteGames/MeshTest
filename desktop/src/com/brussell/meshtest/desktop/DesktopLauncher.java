a0d36f1b7330ae8628746f4e6e20e91f7657cd74
timewastegames <timewastegames@gmail.com>
2017-11-28 20:04:38 +0000

    Add generated project.

---
+++ desktop/src/com/brussell/meshtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brussell.meshtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brussell.meshtest.MeshTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MeshTest(), config);
+	}
+}
